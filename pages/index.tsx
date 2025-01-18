fc0f2b7e005e4d4aab3833d5169dc573581502e9
Rof13thfloor <mojtaba.moazen.a@gmail.com>
2025-01-18 20:09:34 +0100

    on going

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import {NextPage} from 'next';
 
 import About from '../components/About';
+import Story from '../components/Story';
 import PublicationList from '../components/PublicationList';
 import Teaching from '../components/Teaching';
 import Education from '../components/Education';
@@ -9,6 +10,7 @@ import Education from '../components/Education';
 const Index: NextPage<unknown> = () => (
     <>
         <About/>
+        <Story/>
         {/* <PublicationList/> */}
         <Teaching/>
         <Education/>
