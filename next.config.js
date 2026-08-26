8dd6ce2da1e5f69077de7c04f4675a5770e86e3d
Rof13thfloor <mojtaba.moazen.a@gmail.com>
2026-08-26 14:05:04 +0200

    update the website

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const withOffline = require('next-offline');
-
 const nextConfig = {
 	poweredByHeader: false,
-	swcMinify: true,
 };
 
-module.exports = withOffline(nextConfig);
+module.exports = nextConfig;
 
